58e01b43f368fca55fc2e2496f36b5fbcf6212ab
Yutaka Maruyama <drmaruyama@gmail.com>
2026-09-24 10:39:48 +0900

    Set dft = 0.0 in output.cu

--- src-cuda/output.cu
+++ src-cuda/output.cu
@@ -19,7 +19,7 @@ void RISM3D :: output() {
     double * xmu = new double[sv -> natv * 2];
     double * xmu2 = new double[sv -> natv];
     double * se = new double[sv -> natv * 2];
-    double dft;
+    double dft = 0.0;
 
     cal_exchem(xmu, xmu2);
     if (rmdft) dft = cal_rmdft();
